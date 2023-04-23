2ee0a7a3acb407c422bd79d9135486d4e7d0b5fa
Brandon Castro <brandoncastro03@gmail.com>
2023-04-23 03:53:52 -0500

    edited config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId:         'us-east-2_1CGcvaXUS',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '1l0bmc406qbqqup21d73ntm004',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId:         'us-east-2_6ulE7RpxH',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '52ciiog2rr8h5dtfatbuhepuqf',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
-        invokeUrl:          ' https://pchlcjjl23.execute-api.us-east-2.amazonaws.com/prod'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
